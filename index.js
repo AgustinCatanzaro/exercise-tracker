d992bb1c1acd58f46533ecbda7ede8b56308ea11
Agustin Catanzaro <agustincatanzaro@gmail.com>
2023-03-21 18:05:17 -0300

    added folder/file structure

--- index.js
+++ index.js
@@ -3,16 +3,24 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+//connect db
+
+//error handlers
+
+//routers
+
 app.use(cors())
+app.use(express.json())
 app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
+	res.sendFile(__dirname + '/views/index.html')
+})
 
+//endpoints
 
-
-
+//middleware error handlers
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
+	console.log('Your app is listening on port ' + listener.address().port)
 })
