16277708451f90e90d5043b24f0b3d574126bf87
Agustin Catanzaro <agustincatanzaro@gmail.com>
2023-03-21 21:00:51 -0300

    added init and prepared listener to await for db

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const cors = require('cors')
 require('dotenv').config()
 
 //connect db
+// const connectDB = require('./db/connect')
 
 //error handlers
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 //routers
+const exerciseRouter = require('./routes/exercise-tracker')
 
+//middleware
 app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
@@ -18,9 +23,21 @@ app.get('/', (req, res) => {
 })
 
 //endpoints
+app.use('/api', exerciseRouter)
 
 //middleware error handlers
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
-const listener = app.listen(process.env.PORT || 3000, () => {
-	console.log('Your app is listening on port ' + listener.address().port)
-})
+const start = async () => {
+	try {
+		// await connectDB(process.env.MONGO_URI)
+		app.listen(port, () =>
+			console.log(`Server is listening on port ${port}...`)
+		)
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+start()
