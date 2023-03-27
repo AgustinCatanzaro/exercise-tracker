ade8e97cc9f7ae58409e97d25b26caf7c0d76fc4
Agustin Catanzaro <agustincatanzaro@gmail.com>
2023-03-27 16:31:50 -0300

    added body parser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
 require('dotenv').config()
 
 //connect db
@@ -15,6 +16,8 @@ const exerciseRouter = require('./routes/exercise-tracker')
 
 //middleware
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(express.json())
 app.use(express.static('public'))
 
