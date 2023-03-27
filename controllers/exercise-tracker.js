e351c42e52d67f5bb669dc69ebb700c41e86e8b5
Agustin Catanzaro <agustincatanzaro@gmail.com>
2023-03-27 15:33:17 -0300

    added get logs logic

--- controllers/exercise-tracker.js
+++ controllers/exercise-tracker.js
@@ -19,9 +19,6 @@ const createUser = async (req, res) => {
 }
 
 const getAllUsers = async (req, res) => {
-	//RETURN AN ARRAY NO JSON
-	//Each element in the array returned from GET /api/users is an object literal containing a user's username and _id.
-	//Check when sending exercise probably gonna need to change.
 	const usersList = await UserCollection.find()
 	const usernamesArray = new Array()
 
@@ -35,8 +32,6 @@ const getAllUsers = async (req, res) => {
 }
 
 const createExercise = async (req, res) => {
-	//You can POST to /api/users/:_id/exercises with form data description, duration, and optionally date. If no date is supplied, the current date will be used.
-	//The response returned from POST /api/users/:_id/exercises will be the user object with the exercise fields added.
 	const { _id: userId } = req.params
 	const { description, duration } = req.body
 	let { date } = req.body
@@ -84,18 +79,56 @@ const createExercise = async (req, res) => {
 	}
 }
 
-const getLogs = (req, res) => {
-	//You can make a GET request to /api/users/:_id/logs to retrieve a full exercise log of any user.
-	//A request to a user's log GET /api/users/:_id/logs returns a user object with a count property representing the number of exercises that belong to that user.
-	//A GET request to /api/users/:_id/logs will return the user object with a log array of all the exercises added.
-	//Each item in the log array that is returned from GET /api/users/:_id/logs is an object that should have a description, duration, and date properties.
-	//The description property of any object in the log array that is returned from GET /api/users/:_id/logs should be a string.
-	//The duration property of any object in the log array that is returned from GET /api/users/:_id/logs should be a number.
-	//The date property of any object in the log array that is returned from GET /api/users/:_id/logs should be a string. Use the dateString format of the Date API.
-	//You can add from, to and limit parameters to a GET /api/users/:_id/logs request to retrieve part of the log of any user. from and to are dates in yyyy-mm-dd format. limit is an integer of how many logs to send back.
-	res.send('getLogs controller')
-	//Response te Get logs
-	//{"_id":"641dc73e47ca29076a13e64e","username":"zTacaTestingUser","count":3,"log":[{"description":"desctest","duration":99,"date":"Fri Mar 24 2023"},{"description":"desctest","duration":99,"date":"Fri Mar 24 2023"},{"description":"desctest","duration":99,"date":"Fri Mar 24 2023"}]}
+const getLogs = async (req, res) => {
+	//getting the user Data
+	try {
+		const { _id: userId } = req.params
+		let {
+			from: fromDate,
+			to: toDate,
+			limit: amountElementsToShow,
+		} = req.query
+		const { username: name } = await UserCollection.findById(userId)
+
+		//if from/to date where not send in the query we set it to max/min possible date value.
+		if (!fromDate) {
+			fromDate = -8640000000000000
+		}
+		if (!toDate) {
+			toDate = 8640000000000000
+		}
+		//if the limit(amountElementsToShow) doesnt exits, we set it to max and if exist parse it to number
+		!amountElementsToShow
+			? (amountElementsToShow = Number.MAX_SAFE_INTEGER)
+			: (amountElementsToShow = Number(amountElementsToShow))
+
+		const minDateRange = new Date(fromDate)
+		const maxDateRange = new Date(toDate)
+
+		//getting user exercise list
+		let exerciseList = await ExerciseCollection.find({
+			createdBy: userId,
+			date: { $gte: minDateRange, $lte: maxDateRange },
+		}).select('-_id description duration date')
+
+		//mapping the received exercise list to change the date format to string.
+		exerciseList = exerciseList
+			.map(({ description, duration, date }) => {
+				date = date.toDateString()
+				return { description, duration, date }
+			})
+			.slice(0, amountElementsToShow)
+
+		res.status(StatusCodes.OK).json({
+			_id: userId,
+			username: name,
+			count: exerciseList.length,
+			log: exerciseList,
+		})
+	} catch (error) {
+		console.log(error)
+		res.send('something went wrong')
+	}
 }
 
 const staticTesting = async (req, res) => {
@@ -142,8 +175,9 @@ const staticTesting = async (req, res) => {
 	// 	month: 'short',
 	// 	day: 'numeric',
 	// })
-	const testingDate = new Date('Mon Oct 25 1999 21:00:00 GMT-0300')
-	console.log(testingDate.toDateString())
+	const animals = ['ant', 'bison', 'camel', 'duck', 'elephant']
+
+	console.log(animals.slice(0, 3))
 	res.send()
 }
 
